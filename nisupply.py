634d5844725ae4b7ace01ee686b25f4ce3f122b8
Wiesner <joh.wiesner@gmail.com>
2020-11-09 13:31:26 +0100

    allow to save uncompressed files in a specified directory

--- nisupply.py
+++ nisupply.py
@@ -103,14 +103,21 @@ def get_participant_filepaths(participant_ids,participant_dirs,file_extension='.
     return filepaths_df
 
 # TO-DO: Implement this function in get_filepath_df
-def uncompress_files(filepath_list):
+def uncompress_files(filepath_list,dst_dir=None):
     '''Uncompress files and obtain a list of the uncompressed files
     
     Parameters
     ----------
-    filepath_list : list
-        A list of paths pointing to the compressed files. The function
-        assumes that the last extension represents the compression-type.
+    filepath_list : list of str
+        A list of paths of the the compressed files. The function
+        assumes that the filename has excatly two extensions: The first
+        extension represents the native extension of the file, the second extension 
+        represents the compression extension (i.e. 'nii.gz').
+    
+    dst_dir: str
+        A path to a destination directory. If None, uncompressed files are 
+        saved in the source directory of each file. If specified, uncompressed
+        files will be saved in the specified destination directory.
 
     Returns
     -------
@@ -120,26 +127,32 @@ def uncompress_files(filepath_list):
     '''
 
     filepath_list_uncompressed = []
-
+    
     for f in filepath_list:
         
         # get all necessary extensions
-        file_suffixes = pathlib.Path(f).suffixes
-        both_suffixes = ''.join(file_suffixes)
-        file_extension = file_suffixes[0]
-
-        uncompressed_filepath = f.replace(both_suffixes,file_extension)
+        file_extensions = pathlib.Path(f).suffixes
+        file_extension = file_extensions[0]
+        both_extensions = ''.join(file_extensions)
+    
+        filepath_uncompressed = f.replace(both_extensions,file_extension)
+        
+        if dst_dir:
+            dst_dir = os.path.normpath(dst_dir)
+            uncompressed_filename = os.path.basename(filepath_uncompressed)
+            filepath_uncompressed = os.path.join(dst_dir,uncompressed_filename)
         
         # if compressed file already exists do nothing
-        if os.path.exists(uncompressed_filepath):
+        if os.path.exists(filepath_uncompressed):
             pass
         # uncompress file and save it without the compression extension
+        # in either the same folder or, if specfied in a destination directory
         else:
-            with gzip.open(f, 'rb') as f_in:
-                with open(uncompressed_filepath, 'wb') as f_out:
+            with gzip.open(f,'rb') as f_in:
+                with open(filepath_uncompressed,'wb') as f_out:
                     shutil.copyfileobj(f_in,f_out)
             
-        filepath_list_uncompressed.append(uncompressed_filepath)
+        filepath_list_uncompressed.append(filepath_uncompressed)
             
     return filepath_list_uncompressed
 
